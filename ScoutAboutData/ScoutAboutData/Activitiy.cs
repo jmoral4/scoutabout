8a8ada4ff465d495385e44d2c4622a7c854ea2d0
Jonathan <jmoral4@gmail.com>
2013-04-27 13:23:14 -0700

    updates

--- ScoutAboutData/ScoutAboutData/Activitiy.cs
+++ ScoutAboutData/ScoutAboutData/Activitiy.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ScoutAboutData
 {
-    public class Activitiy
+    public class Activity
     {
+        [Required]
         public string Title { get; set; }
         public string PlaceName { get; set; }
         public double Lat { get; set; }
--- ScoutAboutData/ScoutAboutData/Guide.cs
+++ ScoutAboutData/ScoutAboutData/Guide.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ScoutAboutData
 {
     public class Guide
     {
-        public List<Activitiy> Activities { get; set; }
+        public List<Activity> Activities { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Summary { get; set; }
+        [Required]
         public string Author { get; set; }
 
     }
