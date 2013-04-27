84f8b383c1c2deda6f50244de176337d170bb59e
Jonathan <jmoral4@gmail.com>
2013-04-27 13:20:18 -0700

    added activity and guide

---
+++ ScoutAboutData/ScoutAboutData/Activitiy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoutAboutData
+{
+    public class Activitiy
+    {
+        public string Title { get; set; }
+        public string PlaceName { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public decimal Cost { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTimeOffset StartTime { get; set; }
+        public DateTimeOffset EndTime {get;set;}
+        public List<Tag> Tags { get; set; }            
+
+    }
+}
---
+++ ScoutAboutData/ScoutAboutData/Guide.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoutAboutData
+{
+    public class Guide
+    {
+        public List<Activitiy> Activities { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Author { get; set; }
+
+    }
+}
