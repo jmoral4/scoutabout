8e372bfcc2afa5f729ed25f2813f8fead4da7cba
chad downey <mezafits@gmail.com>
2013-04-27 16:42:39 -0700

    test

--- Web/Models/Cities.cs
+++ Web/Models/Cities.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespaceWeb.Models
+namespace Web.Models
 {
-publicclassComboBoxItems
+public class ComboBoxItems
 {
-staticprivateList<string>cities=newList<string>(){"NewYork","LosAngeles","Chicago"," Houston"," Phoenix"," Philadelphia"," SanAntonio"," SanDiego"," Dallas"," SanJose"," Detroit"," Jacksonville"," Indianapolis","SanFrancisco"," Columbus"," Austin"," Memphis"," FortWorth"," Baltimore"," Charlotte"," ElPaso"," Boston"," Seattle"," Washington"," Milwaukee"," Denver"," Louisville/JeffersonCounty"," LasVegas","Nashville-Davidson"," OklahomaCity"," Portland"," Tucson"," Albuquerque"," Atlanta"," LongBeach"," Fresno"," Sacramento"," Mesa"," KansasCity"," Cleveland"," VirginiaBeach"," Omaha"," Miami"," Oakland"," Tulsa"," Honolulu"," Minneapolis"," ColoradoSprings"," Arlington"," Wichita"," Raleigh"," St.Louis"," SantaAna"," Anaheim"," Tampa"," Cincinnati"," Pittsburgh"," Bakersfield"," Aurora"," Toledo"," Riverside"," Stockton"," CorpusChristi"," Newark"," Anchorage","Buffalo"," St.Paul"," Lexington-Fayette","Plano"," FortWayne"," St.Petersburg"," Glendale"," JerseyCity"," Lincoln"," Henderson"," Chandler"," Greensboro"," Scottsdale"," BatonRouge"," Birmingham"," Norfolk"," Madison"," NewOrleans"," Chesapeake"," Orlando","Garland","Hialeah","Laredo"," ChulaVista"," Lubbock"," Reno"," Akron"," Durham"," Rochester"," Modesto"," Montgomery"," Fremont"," Shreveport"," Arlington","Glendale"};
-staticpublicList<string>Cities{get{
+static private List<string>cities= new List<string>(){"NewYork","LosAngeles","Chicago"," Houston"," Phoenix"," Philadelphia"," SanAntonio"," SanDiego"," Dallas"," SanJose"," Detroit"," Jacksonville"," Indianapolis","SanFrancisco"," Columbus"," Austin"," Memphis"," FortWorth"," Baltimore"," Charlotte"," ElPaso"," Boston"," Seattle"," Washington"," Milwaukee"," Denver"," Louisville/JeffersonCounty"," LasVegas","Nashville-Davidson"," OklahomaCity"," Portland"," Tucson"," Albuquerque"," Atlanta"," LongBeach"," Fresno"," Sacramento"," Mesa"," KansasCity"," Cleveland"," VirginiaBeach"," Omaha"," Miami"," Oakland"," Tulsa"," Honolulu"," Minneapolis"," ColoradoSprings"," Arlington"," Wichita"," Raleigh"," St.Louis"," SantaAna"," Anaheim"," Tampa"," Cincinnati"," Pittsburgh"," Bakersfield"," Aurora"," Toledo"," Riverside"," Stockton"," CorpusChristi"," Newark"," Anchorage","Buffalo"," St.Paul"," Lexington-Fayette","Plano"," FortWayne"," St.Petersburg"," Glendale"," JerseyCity"," Lincoln"," Henderson"," Chandler"," Greensboro"," Scottsdale"," BatonRouge"," Birmingham"," Norfolk"," Madison"," NewOrleans"," Chesapeake"," Orlando","Garland","Hialeah","Laredo"," ChulaVista"," Lubbock"," Reno"," Akron"," Durham"," Rochester"," Modesto"," Montgomery"," Fremont"," Shreveport"," Arlington","Glendale"};
+static public List<string> Cities{get{
 cities.Sort();
-returncities;}}
+return cities;}}
 }
 }
