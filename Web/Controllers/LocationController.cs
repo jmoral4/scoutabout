306ece20f8f5e72c871c58b8ca8cbdfef0be2936
chad downey <mezafits@gmail.com>
2013-04-27 19:04:30 -0700

    adding components for details city view

---
+++ Web/Controllers/LocationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class LocationController : Controller
+    {
+        //
+        // GET: /Location/
+
+        public ActionResult Index()
+        {
+            List<Web.Models.Location> locations = new List<Models.Location>();
+            locations.Add(new Models.Location() { City = "San Francisco", Image = Url.Content("..\\Images\\Sanfran.png"), State = "CA", Summary = "Home of the Golden Gate bridge and south of the Red Wood Forest. Oldest Chinatown in the US, classic city parks and fresh made sourdough bread.", Zipcode = "96789" });
+            locations.Add(new Models.Location(){ City = "Portland", Image = Url.Content("..\\Images\\Portland.png"), State = "OR", Summary = "Home of the Golden Gate bridge and south of the Red Wood Forest. Oldest Chinatown in the US, classic city parks and fresh made sourdough bread.", Zipcode = "96789"});
+            locations.Add(new Models.Location(){ City = "Chicago", Image = Url.Content("..\\Images\\Chicago.png"), State = "IL", Summary = "Home of the Golden Gate bridge and south of the Red Wood Forest. Oldest Chinatown in the US, classic city parks and fresh made sourdough bread.", Zipcode = "96789"});
+            return PartialView("Partials/LocationPartial",locations);
+        }
+
+    }
+}
--- Web/Models/Cities.cs
+++ Web/Models/Cities.cs
@@ -5,6 +5,7 @@ using System.Web;
 
 namespace Web.Models
 {
+
 public class ComboBoxItems
 {
 static private List<string>cities= new List<string>(){"NewYork","LosAngeles","Chicago"," Houston"," Phoenix"," Philadelphia"," SanAntonio"," SanDiego"," Dallas"," SanJose"," Detroit"," Jacksonville"," Indianapolis","SanFrancisco"," Columbus"," Austin"," Memphis"," FortWorth"," Baltimore"," Charlotte"," ElPaso"," Boston"," Seattle"," Washington"," Milwaukee"," Denver"," Louisville/JeffersonCounty"," LasVegas","Nashville-Davidson"," OklahomaCity"," Portland"," Tucson"," Albuquerque"," Atlanta"," LongBeach"," Fresno"," Sacramento"," Mesa"," KansasCity"," Cleveland"," VirginiaBeach"," Omaha"," Miami"," Oakland"," Tulsa"," Honolulu"," Minneapolis"," ColoradoSprings"," Arlington"," Wichita"," Raleigh"," St.Louis"," SantaAna"," Anaheim"," Tampa"," Cincinnati"," Pittsburgh"," Bakersfield"," Aurora"," Toledo"," Riverside"," Stockton"," CorpusChristi"," Newark"," Anchorage","Buffalo"," St.Paul"," Lexington-Fayette","Plano"," FortWayne"," St.Petersburg"," Glendale"," JerseyCity"," Lincoln"," Henderson"," Chandler"," Greensboro"," Scottsdale"," BatonRouge"," Birmingham"," Norfolk"," Madison"," NewOrleans"," Chesapeake"," Orlando","Garland","Hialeah","Laredo"," ChulaVista"," Lubbock"," Reno"," Akron"," Durham"," Rochester"," Modesto"," Montgomery"," Fremont"," Shreveport"," Arlington","Glendale"};
